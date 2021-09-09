8ca30c86fe0caac3d2e47363ec2295c2b649220a
hepimen <mhrzstudio@gmail.com>
2021-09-09 16:41:16 +0700

    day 1-1

---
+++ scripts/multiple-2.js
@@ -0,0 +1,13 @@
+const company = "Similikiti CV";
+
+
+function sum(first, second) {
+    return first + second;
+}
+
+class Company {
+
+}
+
+
+export { company, sum, Company };
---
+++ scripts/multiple.js
@@ -0,0 +1,16 @@
+const company = "Similikiti CV";
+
+
+function sum(first, second) {
+    return first + second;
+}
+
+class Company {
+
+}
+
+
+export { company, sum, Company };
+
+// using alias
+// export { company as perusahaan, sum as total, Company as Perusahaan };
