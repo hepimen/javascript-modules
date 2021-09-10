680dd77fa5cc8fa13cfe2abb2b0bcbd8ce6c5a17
hepimen <mhrzstudio@gmail.com>
2021-09-10 18:36:13 +0700

    finish

---
+++ scripts/module-object.js
@@ -0,0 +1,19 @@
+export const firstName = "Jams";
+export const lastName = "Kurniawan";
+export const middleName = "Pardede";
+
+
+export function hello() {
+    console.info(`Helllo from Jams`);
+}
+
+export class Person {
+    constructor(name) {
+        this.name = "Eko";
+    }
+
+    sayHello() {
+        console.info(`halo ${this.name}`);
+    }
+}
+
